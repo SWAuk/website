6f1be79d6b86650ca652c44a03740e76933c3a90
addshore <addshorewiki@gmail.com>
2014-07-22 14:06:06 +0100

    Add custom user fields plugin

--- com_swa/admin/sql/install.mysql.utf8.sql
+++ com_swa/admin/sql/install.mysql.utf8.sql
@@ -3,15 +3,3 @@ See http://docs.joomla.org/J3.x:Developing_a_MVC_Component/Using_the_database
 This code is run on install
 This file should be 'utf8 NO BOM'
 */
-
-DROP TABLE IF EXISTS `#__helloworld`;
-
-CREATE TABLE `#__helloworld` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `greeting` varchar(25) NOT NULL,
-   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-
-INSERT INTO `#__helloworld` (`greeting`) VALUES
-        ('Hello World!'),
-        ('Good bye World!');
--- com_swa/admin/sql/uninstall.mysql.utf8.sql
+++ com_swa/admin/sql/uninstall.mysql.utf8.sql
@@ -3,3 +3,5 @@ See http://docs.joomla.org/J3.x:Developing_a_MVC_Component/Using_the_database
 This code is run on uninstall
 This file should be 'utf8 NO BOM'
 */
+
+DROP TABLE IF EXISTS `#__helloworld`;
