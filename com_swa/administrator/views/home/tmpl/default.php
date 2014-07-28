a49719fa075b75029ed6825c99264c98766466c9
addshore <addshorewiki@gmail.com>
2014-07-28 12:03:56 +0100

    Enhance admin home view

--- com_swa/administrator/views/home/tmpl/default.php
+++ com_swa/administrator/views/home/tmpl/default.php
@@ -1,5 +1,20 @@
 <?php
 // No direct access to this file
 defined( '_JEXEC' ) or die( 'Restricted access' );
+
 ?>
-<h1><?php echo $this->msg; ?></h1>
+
+<?php if (!empty( $this->sidebar )): ?>
+<div id="j-sidebar-container" class="span2">
+	<?php echo $this->sidebar; ?>
+</div>
+<div id="j-main-container" class="span10">
+	<?php else : ?>
+	<div id="j-main-container">
+		<?php endif; ?>
+
+		<h1><?php echo $this->header; ?></h1>
+
+		<p><?php echo $this->message; ?></p>
+
+	</div>
--- com_swa/administrator/views/home/view.html.php
+++ com_swa/administrator/views/home/view.html.php
@@ -11,8 +11,15 @@ class SwaViewHome extends JViewLegacy {
 
 	// Overwriting JView display method
 	function display( $tpl = null ) {
+
+		JToolBarHelper::title( JText::_( 'SWA home' ) );
+
+		SwaHelper::addSubmenu( 'home' );
+		$this->sidebar = JHtmlSidebar::render();
+
 		// Assign data to the view
-		$this->msg = 'SWA home... (use the menu)';
+		$this->header = 'Welcome!';
+		$this->message = 'Nothing is here yet, please use the menu to select the correct view.';
 
 		// Display the view
 		parent::display( $tpl );
