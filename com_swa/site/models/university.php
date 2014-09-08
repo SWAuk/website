2c74c9a5c6eef5b599bb795ff58c9607f27f56b1
addshore <addshorewiki@gmail.com>
2014-09-08 15:51:04 +0100

    VIEW members can view their university info
    Fixes #10

---
+++ com_swa/site/models/university.php
@@ -0,0 +1,44 @@
+<?php
+
+// No direct access.
+defined( '_JEXEC' ) or die;
+
+jimport( 'joomla.application.component.modeladmin' );
+
+class SwaModelUniversity extends JModelItem {
+
+	/**
+	 * @param string $type
+	 * @param string $prefix
+	 * @param array $config
+	 * @return JTable
+	 */
+	public function getTable( $type = 'University', $prefix = 'SwaTable', $config = array() ) {
+		return JTable::getInstance( $type, $prefix, $config );
+	}
+
+	/**
+	 * @return JTable
+	 */
+	public function getItem() {
+		// Create a new query object.
+		$db = $this->getDbo();
+		$query = $db->getQuery(true);
+		$user = JFactory::getUser();
+
+		// Select the required fields from the table.
+		$query->select( 'a.university_id AS university_id' );
+		$query->from( $db->quoteName('#__swa_member') . ' AS a' );
+		$query->where( 'a.user_id = ' . $user->id );
+		// Join over the university
+		$query->select( 'b.name AS university' );
+		$query->select( 'b.url AS url' );
+		$query->select( 'b.password AS password' );
+		$query->join( 'LEFT', '#__swa_university AS b ON a.university_id = b.id' );
+
+		// Load the result
+		$db->setQuery($query);
+		return $db->loadObject();
+	}
+
+}
---
+++ com_swa/site/views/university/tmpl/default.php
@@ -0,0 +1,60 @@
+<?php
+
+// no direct access
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.keepalive');
+JHtml::_('behavior.tooltip');
+JHtml::_('behavior.formvalidation');
+JHtml::_('formbehavior.chosen', 'select');
+
+//Load admin language file
+$lang = JFactory::getLanguage();
+$lang->load('com_swa', JPATH_ADMINISTRATOR);
+$doc = JFactory::getDocument();
+$doc->addScript(JUri::base() . '/components/com_swa/assets/js/form.js');
+?>
+
+<!--</style>-->
+<script type="text/javascript">
+	getScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function() {
+		jQuery(document).ready(function() {
+			jQuery('#form-member').submit(function(event) {
+			});
+		});
+	});
+</script>
+
+<h1>Your University Details</h1>
+
+<table>
+
+	<tr>
+		<div class="control-group">
+			<td>University id:</td>
+			<td><?php echo $this->item->university_id ?></td>
+		</div>
+	</tr>
+
+	<tr>
+		<div class="control-group">
+			<td>University:</td>
+			<td><?php echo $this->item->university ?></td>
+		</div>
+	</tr>
+
+	<tr>
+		<div class="control-group">
+			<td>Url:</td>
+			<td><?php echo ( $this->item->url ) ? 'Yes' : 'No' ?></td>
+		</div>
+	</tr>
+
+	<tr>
+		<div class="control-group">
+			<td>Password:</td>
+			<td><?php echo $this->item->password ?></td>
+		</div>
+	</tr>
+
+</table>
