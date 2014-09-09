355b01022dae65a9a2e46a292ef97ed14a891bc4
addshore <addshorewiki@gmail.com>
2014-09-09 14:33:25 +0100

    Add frontend members view Fixes #16

---
+++ com_swa/site/models/members.php
@@ -0,0 +1,118 @@
+<?php
+
+defined( '_JEXEC' ) or die;
+
+jimport( 'joomla.application.component.modellist' );
+
+/**
+ * Methods supporting a list of Swa records.
+ */
+class SwaModelMembers extends JModelList {
+
+	protected $member;
+
+	/**
+	 * @return JTable|mixed
+	 */
+	public function getMember() {
+		if( !isset( $this->member ) ) {
+			// Create a new query object.
+			$db = $this->getDbo();
+			$query = $db->getQuery(true);
+			$user = JFactory::getUser();
+
+			// Select the required fields from the table.
+			$query->select( 'a.*' );
+			$query->from( $db->quoteName('#__swa_member') . ' AS a' );
+			$query->where( 'a.user_id = ' . $user->id );
+
+			// Load the result
+			$db->setQuery($query);
+			$this->member = $db->loadObject();
+		}
+		return $this->member;
+	}
+
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 */
+	protected function populateState( $ordering = null, $direction = null ) {
+		// Initialise variables.
+		$app = JFactory::getApplication();
+
+		// Load the filter state.
+		$search = $app->getUserStateFromRequest( $this->context . '.filter.search', 'filter_search' );
+		$this->setState( 'filter.search', $search );
+
+		// Load the parameters.
+		$params = JComponentHelper::getParams( 'com_swa' );
+		$this->setState( 'params', $params );
+
+		// List state information.
+		parent::populateState( 'a.id', 'asc' );
+	}
+
+	/**
+	 * Method to get a store id based on model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param    string $id A prefix for the store id.
+	 *
+	 * @return    string        A store id.
+	 * @since    1.6
+	 */
+	protected function getStoreId( $id = '' ) {
+		// Compile the store id.
+		$id .= ':' . $this->getState( 'filter.search' );
+
+		return parent::getStoreId( $id );
+	}
+
+	/**
+	 * Build an SQL query to load the list data.
+	 *
+	 * @return    JDatabaseQuery
+	 * @since    1.6
+	 */
+	protected function getListQuery() {
+		// Create a new query object.
+		$db = $this->getDbo();
+		$query = $db->getQuery( true );
+
+		// Select the required fields from the table.
+		$query->select(
+			$this->getState(
+				'list.select', 'DISTINCT a.*'
+			)
+		);
+		$query->from( '`#__swa_member` AS a' );
+
+		// Join over the user field 'user_id'
+		$query->select( 'user_id.name AS user' );
+		$query->join( 'LEFT', '#__users AS user_id ON user_id.id = a.user_id' );
+		// Join over the user field 'university_id'
+		$query->select( 'university_id.name AS university' );
+		$query->join( 'LEFT', '#__swa_university AS university_id ON university_id.id = a.university_id' );
+
+		// Add the list ordering clause.
+		$orderCol = $this->state->get( 'list.ordering' );
+		$orderDirn = $this->state->get( 'list.direction' );
+		if ( $orderCol && $orderDirn ) {
+			$query->order( $db->escape( $orderCol . ' ' . $orderDirn ) );
+		}
+
+		return $query;
+	}
+
+	public function getItems() {
+		$items = parent::getItems();
+
+		return $items;
+	}
+
+}
---
+++ com_swa/site/views/members/view.html.php
@@ -0,0 +1,65 @@
+<?php
+
+// No direct access
+defined( '_JEXEC' ) or die;
+
+jimport( 'joomla.application.component.view' );
+
+/**
+ * View class for a list of Swa.
+ */
+class SwaViewMembers extends JViewLegacy {
+
+	protected $items;
+	protected $pagination;
+	protected $state;
+	protected $user;
+	protected $member;
+
+	/**
+	 * Display the view
+	 */
+	public function display( $tpl = null ) {
+		$app = JFactory::getApplication();
+
+		$this->state = $this->get( 'State' );
+		$this->pagination = $this->get( 'Pagination' );
+
+		// Check for errors.
+		if ( count( $errors = $this->get( 'Errors' ) ) ) {
+			throw new Exception( implode( "\n", $errors ) );
+		}
+
+		$this->user = JFactory::getUser();
+
+		// If not logged in
+		if( $this->user->id === 0 ) {
+			$url = 'index.php?option=com_users';
+			$url.= '&return=' . base64_encode( JURI::getInstance()->toString() );
+			$app->redirect( JRoute::_( $url, false ) );
+		}
+
+		$this->member = $this->get( 'Member' );
+		if ( !$this->member->paid ) {
+			$app->redirect( JRoute::_( 'index.php?option=com_swa&view=memberpayment' ) );
+		}
+		elseif( !$this->member->swa_committee ) {
+			//TODO do something better here
+			die( 'You need to be an SWA committee member to view this page..' );
+		}
+
+		$this->items = $this->get( 'Items' );
+
+		parent::display( $tpl );
+	}
+
+
+	protected function getSortFields() {
+		return array(
+			'a.id' => JText::_( 'JGRID_HEADING_ID' ),
+			'a.user' => JText::_( 'User' ),
+			'a.university' => JText::_( 'University' ),
+		);
+	}
+
+}
