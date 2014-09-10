afc9df2c6ec0569ad9746ddafc00823da58b8d80
addshore <addshorewiki@gmail.com>
2014-09-10 16:49:21 +0100

    Add basic code for front ticketpurchase view #8

---
+++ com_swa/site/models/ticketpurchase.php
@@ -0,0 +1,67 @@
+<?php
+
+// No direct access.
+defined( '_JEXEC' ) or die;
+
+jimport( 'joomla.application.component.modeladmin' );
+
+class SwaModelTicketPurchase extends JModelList {
+
+	protected $member;
+
+	/**
+	 * @return JTable|mixed
+	 */
+	public function getMember() {
+		if( !isset( $this->member ) ) {
+			// Create a new query object.
+			$db = $this->getDbo();
+			$query = $db->getQuery(true);
+			$user = JFactory::getUser();
+
+			// Select the required fields from the table.
+			$query->select( 'a.*' );
+			$query->from( $db->quoteName('#__swa_member') . ' AS a' );
+			$query->where( 'a.user_id = ' . $user->id );
+
+			// Load the result
+			$db->setQuery($query);
+			$this->member = $db->loadObject();
+		}
+		return $this->member;
+	}
+
+	public function getListQuery() {
+		$db = $this->getDbo();
+		$query = $db->getQuery(true);
+
+		// Select all event tickets
+		$query->select( 'a.id as id' );
+		$query->from( $db->quoteName('#__swa_event_ticket') . ' AS a' );
+
+		// Select details of the event
+		$query->innerJoin( '#__swa_event as event ON a.event_id=event.id' );
+		$query->select( 'event.name as event' );
+		$query->select( 'event.date as event_date' );
+		$query->select( 'event.date_close as event_close' );
+
+		// TODO Where event_close has not passed
+
+		// Where we still have tickets remaining
+		$subQuery = $db->getQuery(true);
+		$subQuery->select( 'COUNT( ticket.id )' );
+		$subQuery->from( '#__swa_ticket as ticket' );
+		$subQuery->where( 'ticket.event_ticket_id=a.id' );
+		$query->where( '( a.quantity - ( ' . $subQuery->__toString() . ' ) ) > 0' );
+
+		return $query;
+	}
+
+	public function getItems() {
+		$items = parent::getItems();
+		//TODO make sure member is currently registered to buy tickets
+		//TODO make sure current member can buy said ticket
+		return $items;
+	}
+
+}
---
+++ com_swa/site/views/ticketpurchase/tmpl/default.php
@@ -0,0 +1,60 @@
+<?php
+
+// no direct access
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.keepalive');
+JHtml::_('behavior.tooltip');
+JHtml::_('behavior.formvalidation');
+JHtml::_('formbehavior.chosen', 'select');
+
+//Load admin language file
+$lang = JFactory::getLanguage();
+$lang->load('com_swa', JPATH_ADMINISTRATOR);
+$doc = JFactory::getDocument();
+$doc->addScript(JUri::base() . '/components/com_swa/assets/js/form.js');
+?>
+
+<!--</style>-->
+<script type="text/javascript">
+	getScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function() {
+		jQuery(document).ready(function() {
+			jQuery('#form-member').submit(function(event) {
+			});
+		});
+	});
+</script>
+
+<h1>Ticket Purchasing</h1>
+
+<?php
+if ( empty( $this->items ) ) {
+	echo "<p><b>There are currently no tickets that you can buy!</b></p>";
+} else {
+	?>
+	<table>
+		<tr>
+			<th>Event</th>
+			<th>Date</th>
+			<th>Deadline</th>
+			<th>Ticket</th>
+			<th>Buy</th>
+		</tr>
+
+		<?php
+		foreach( $this->items as $item ) {
+			echo "<tr>\n";
+			echo "<td>" . $item->event . "</td>\n";
+			echo "<td>" . $item->event_date . "</td>\n";
+			echo "<td>" . $item->event_close . "</td>\n";
+			echo "<td>" . $item->ticket . "</td>\n";
+			//TODO buy me link / nochex button!
+			echo "<td>Buy me</td>\n";
+			echo "</tr>\n";
+		}
+		?>
+
+	</table>
+	<?php
+}
+?>
