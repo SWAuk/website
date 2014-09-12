9f76942059e15e17a18d541dffe53509b3d07d38
addshore <addshorewiki@gmail.com>
2014-09-12 13:43:01 +0100

    DROP all created tables on uninstall

--- com_swa/administrator/sql/uninstall.mysql.utf8.sql
+++ com_swa/administrator/sql/uninstall.mysql.utf8.sql
@@ -1,10 +1,13 @@
 DROP TABLE IF EXISTS `#__swa_member`;
+DROP TABLE IF EXISTS `#__swa_instructor`;
 DROP TABLE IF EXISTS `#__swa_university`;
+DROP TABLE IF EXISTS `#__swa_university_member`;
 DROP TABLE IF EXISTS `#__swa_season`;
 DROP TABLE IF EXISTS `#__swa_deposit`;
 DROP TABLE IF EXISTS `#__swa_ticket_type`;
 DROP TABLE IF EXISTS `#__swa_event`;
 DROP TABLE IF EXISTS `#__swa_event_host`;
+DROP TABLE IF EXISTS `#__swa_event_registration`;
 DROP TABLE IF EXISTS `#__swa_event_ticket`;
 DROP TABLE IF EXISTS `#__swa_grant`;
 DROP TABLE IF EXISTS `#__swa_ticket`;
