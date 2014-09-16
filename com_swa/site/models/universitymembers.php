635a44f5e5638de640732a3e440b6abb318bacc0
addshore <addshorewiki@gmail.com>
2014-09-16 17:15:47 +0100

    Remove some dead code

--- com_swa/site/models/universitymembers.php
+++ com_swa/site/models/universitymembers.php
@@ -49,16 +49,10 @@ class SwaModelUniversityMembers extends JModelList {
 		$query->select( 'member.*' );
 		$query->from( $db->quoteName('#__swa_member') . ' AS member' );
 		$query->where( 'member.university_id = ' . $this->getMember()->university_id );
-		$query->group( 'member.id' );
 
 		// Join onto joomla user table
 		$query->select( 'user.name AS name' );
 		$query->join( 'LEFT', '#__users AS user ON member.user_id = user.id' );
-
-		//Get a concat list of event ids registered to
-		//TODO only get registered ids where date isnt over 3 days ago
-		$query->select( 'GROUP_CONCAT( event_registration.event_id ) as registered_event_ids' );
-		$query->join( 'LEFT', '#__swa_event_registration AS event_registration ON member.id = event_registration.member_id' );
 
 		return $query;
 	}
