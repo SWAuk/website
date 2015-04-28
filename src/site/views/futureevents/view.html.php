5ed7f51ba64ce3c8a059df089a0b7b5bc8751b78
addshore <addshorewiki@gmail.com>
2015-04-28 11:18:48 +0100

    Add views foe future & Past Events
    Fixes #5
    Fixes #6

---
+++ src/site/views/futureevents/view.html.php
@@ -0,0 +1,33 @@
+<?php
+
+// No direct access
+defined( '_JEXEC' ) or die;
+
+jimport( 'joomla.application.component.view' );
+
+class SwaViewFutureEvents extends JViewLegacy {
+
+	protected $state;
+	protected $items;
+	protected $params;
+
+	/**
+	 * Display the view
+	 */
+	public function display( $tpl = null ) {
+		$app = JFactory::getApplication();
+
+		$this->state = $this->get( 'State' );
+		$this->params = $app->getParams( 'com_swa' );
+
+		// Check for errors.
+		if ( count( $errors = $this->get( 'Errors' ) ) ) {
+			throw new Exception( implode( "\n", $errors ) );
+		}
+
+		$this->items = $this->get( 'Items' );
+
+		parent::display( $tpl );
+	}
+
+}
