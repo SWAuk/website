a16fcba2f0856daa175f3c230c35055b6f6bfd68
addshore <addshorewiki@gmail.com>
2015-10-02 21:35:01 +0100

    Require approved qualifications to buy tickets

--- src/site/models/ticketpurchase.php
+++ src/site/models/ticketpurchase.php
@@ -40,6 +40,7 @@ class SwaModelTicketPurchase extends SwaModelList {
 			$subQuery->select( 'COUNT(*)' );
 			$subQuery->from( $db->quoteName( '#__swa_qualification' ) . ' AS qualification' );
 			$subQuery->where( 'qualification.expiry_date > NOW()' );
+			$subQuery->where( 'qualification.approved=1' );
 			$query->select( '(' . $subQuery->__toString() . ') as qualification' );
 
 			// Get event ids registered for!
