b864bdc9b3e39338e439e341b4bc65ccc5cc97f1
addshore <addshorewiki@gmail.com>
2015-10-02 18:24:01 +0100

    Add empty qualfication controller with todos

---
+++ src/site/controllers/orgmemberqualifications.php
@@ -0,0 +1,20 @@
+<?php
+
+// No direct access
+defined( '_JEXEC' ) or die;
+
+require_once JPATH_COMPONENT . '/controller.php';
+
+class SwaControllerOrgMemberQualifications extends SwaController {
+
+	public function delete() {
+		//TODO implement me
+		die( 'not yet implemented' );
+	}
+
+	public function add() {
+		//TODO implement me
+		die( 'not yet implemented' );
+	}
+
+}
