a3ed021bc51bba8bb3509783e766dab267d70cd6
addshore <addshore@users.noreply.github.com>
2017-11-01 21:05:29 +0000

    LIVEFIX correct sold ticket quantity

--- src/site/models/ticketpurchase.php
+++ src/site/models/ticketpurchase.php
@@ -75,36 +75,60 @@ class SwaModelTicketPurchase extends SwaModelList {
 		$db = $this->getDbo();
 		$query = $db->getQuery( true );
 
-		// Select all event tickets
-		$query->select( 'event_ticket.id AS id' );
-		$query->select( 'event_ticket.name AS ticket_name' );
-		$query->select( 'event_ticket.price AS price' );
-		$query->select( 'event_ticket.notes AS notes' );
-		$query->select( 'event_ticket.need_level AS need_level' );
-		$query->select( 'event_ticket.need_xswa AS need_xswa' );
-		$query->select( 'event_ticket.need_swa AS need_swa' );
-		$query->select( 'event_ticket.need_host AS need_host' );
-		$query->select( 'event_ticket.need_qualification AS need_qualification' );
-		$query->select( 'event_ticket.details AS details' );
-
-		$query->select( 'event.id AS event_id' );
+		# Adam XXX, Added lines below to cover the change in var!!!
+		$query->select( 'a.details AS details' );
 		$query->select( 'event.name AS event_name' );
-		$query->select( 'event.date AS event_date' );
 		$query->select( 'event.date_open AS ticket_open' );
 		$query->select( 'event.date_close AS ticket_close' );
 		$query->select( 'event.capacity AS event_capacity' );
-		$query->select( 'event_ticket.quantity AS ticket_quantity' );
-		$query->select( 'COUNT(ticket.id) AS tickets_sold' );
-		$query->select( 'GROUP_CONCAT( DISTINCT event_host.university_id ) AS host_university_ids' );
-
-		$query->from( '#__swa_event_ticket AS event_ticket' );
-		$query->innerJoin( '#__swa_event AS event ON event_ticket.event_id = event.id' );
-		$query->leftJoin( '#__swa_event_host AS event_host ON event_host.event_id = event.id' );
-		$query->leftJoin( '#__swa_ticket AS ticket ON ticket.event_ticket_id = event_ticket.id' );
-
+		$query->select( 'a.quantity AS ticket_quantity' );
+		#$query->select( 'COUNT(ticket.id) AS tickets_sold' );
+		
+		// Select all event tickets
+		$query->select( 'a.id as id' );
+		$query->select( 'a.name as ticket_name' );
+		$query->select( 'a.price as price' );
+		$query->select( 'a.notes as notes' );
+		$query->select( 'a.need_level as need_level' );
+		$query->select( 'a.need_xswa as need_xswa' );
+		$query->select( 'a.need_swa as need_swa' );
+		$query->select( 'a.need_host as need_host' );
+		$query->select( 'a.need_qualification as need_qualification' );
+		$query->from( $db->quoteName( '#__swa_event_ticket' ) . ' AS a' );
+		// Select details of the event
+		$query->innerJoin( '#__swa_event as event ON a.event_id=event.id' );
+		$query->select( 'event.name as event' );
+		$query->select( 'event.id as event_id' );
+		$query->select( 'event.date as event_date' );
+		$query->select( 'event.date_close as event_close' );
+		$query->select( 'event.capacity as capacity' );
+		// Where we still have capacity left in the event
+		$subQuerySold = $db->getQuery( true );
+		$subQuerySold->select( 'COUNT( ticket.id )' );
+		$subQuerySold->from( '#__swa_ticket as ticket' );
+		$subQuerySold->rightJoin( '#__swa_event_ticket AS event_ticket ON ticket.event_ticket_id = event_ticket.id' );
+		$subQuerySold->where( 'event_ticket.event_id=event.id' );
+		$query->select( '( ' . $subQuerySold->__toString() . ' ) as sold' );
+		$query->select( '( event.capacity - ( ' . $subQuerySold->__toString() . ' ) ) as capacity_remaining' );
+		// Select details of the event hosts
+		$query->join(
+			'LEFT',
+			'#__swa_event_host AS event_host ON event_host.event_id = a.event_id'
+		);
+		$query->select( 'GROUP_CONCAT( event_host.university_id ) as host_university_ids' );
+		$query->group( 'a.id' );
+		// Select event closed & open indicators
+		$query->select( '( event.date_close < NOW() ) as event_has_closed' );
+		$query->select( '( event.date_open < NOW() ) as event_has_opened' );
+		// Where the event is in the future!
 		$query->where( 'event.date > NOW()' );
-		$query->group( 'id' );
-		$query->order('event_id ASC', 'id ASC');
+		// Where we still have tickets remaining
+		$subQuery = $db->getQuery( true );
+		$subQuery->select( 'COUNT( ticket.id )' );
+		$subQuery->from( '#__swa_ticket as ticket' );
+		$subQuery->where( 'ticket.event_ticket_id=a.id' );
+		$query->select( '( a.quantity - ( ' . $subQuery->__toString() . ' ) ) as quantity_remaining' );
+		$query->select( '( ( ' . $subQuery->__toString() . ' ) ) as tickets_sold' );
 
 		return $query;
 	}
