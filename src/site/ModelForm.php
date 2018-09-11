16d28b98d9e79b9b1ea1fa904768cc908b245e2c
Oliver Shotton <geekman92@gmail.com>
2018-09-11 23:00:23 +0100

    Updated getMember methods in model bases

--- src/site/ModelForm.php
+++ src/site/ModelForm.php
@@ -1,43 +1,57 @@
 <?php
 
-abstract class SwaModelForm extends JModelForm {
+abstract class SwaModelForm extends JModelForm
+{
 
-	/**
-	 * @var JTable
-	 */
-	protected $member;
+    /**
+     * @var JTable
+     */
+    protected $member;
 
-	/**
-	 * @return JTable|mixed
-	 */
-	public function getMember() {
-		if ( !isset( $this->member ) ) {
-			// Create a new query object.
-			$db = $this->getDbo();
-			$query = $db->getQuery( true );
-			$user = JFactory::getUser();
+    /**
+     * @return JTable|mixed
+     */
+    public function getMember()
+    {
+        if (!isset($this->member)) {
+            // Create a new query object.
+            $db = $this->getDbo();
+            $query = $db->getQuery(true);
+            $user = JFactory::getUser();
 
-			// Select the required fields from the table.
-			$query->select( 'a.*' );
-			$query->from( $db->quoteName( '#__swa_member' ) . ' AS a' );
-			$query->where( 'a.user_id = ' . $user->id );
+            // Select the required fields from the table.
+            $query->select('a.*');
+            $query->from('#__swa_member AS a');
+            $query->where('a.user_id = ' . (int)$user->id);
 
-			// Join on committee table
-			$query->leftJoin( '#__swa_committee as committee on committee.member_id = a.id' );
-			$query->select( '!ISNULL(committee.id) as swa_committee' );
+            // Join on committee table
+            $query->leftJoin('#__swa_committee AS committee ON committee.member_id = a.id');
+            $query->select('!ISNULL(committee.id) AS swa_committee');
 
-			// Join on committee table
-			$query->leftJoin(
-				'#__swa_university_member as uni_member on uni_member.member_id = a.id'
-			);
-			$query->select( 'uni_member.committee as club_committee' );
+            // Join on university_member table
+            $query->leftJoin('#__swa_university_member AS uni_member ON uni_member.member_id = a.id');
+            $query->select('uni_member.committee AS club_committee');
 
-			// Load the result
-			$db->setQuery( $query );
-			$this->member = $db->loadObject();
-		}
+            $now = time();
+            $seasonEnd = strtotime("1st June");
+            $date = $now < $seasonEnd ? date("Y", strtotime('-1 year', $now)) : date("Y", $now);
+            // Join on membership table
+            $query->leftJoin('#__swa_membership AS membership ON membership.member_id = a.id');
+            $query->select('membership.season_id');
+            $query->leftJoin('#__swa_season AS season ON membership.season_id = season.id');
+            $query->where('(season.year LIKE "' . (int)$date . '%" OR membership.season_id IS NULL)');
 
-		return $this->member;
-	}
+            // Load the result
+            $db->setQuery($query);
+            $this->member = $db->loadObject();
+
+            //
+            if ($this->member !== null) {
+                $this->member->paid = $this->member->season_id != null || $this->member->lifetime_member;
+            }
+        }
+
+        return $this->member;
+    }
 
 }
