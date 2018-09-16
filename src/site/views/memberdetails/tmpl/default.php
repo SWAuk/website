0fde7d556ddadee2525956e7facd76e934d92c39
Oliver Shotton <geekman92@gmail.com>
2018-09-16 22:07:16 +0100

    Update memberdetails for anual membership change

--- src/site/views/memberdetails/tmpl/default.php
+++ src/site/views/memberdetails/tmpl/default.php
@@ -1,17 +1,17 @@
 <?php
 
-defined('_JEXEC') or die;
+defined( '_JEXEC' ) or die;
 
-JHtml::_('behavior.keepalive');
-JHtml::_('behavior.tooltip');
-JHtml::_('behavior.formvalidation');
-JHtml::_('formbehavior.chosen', 'select');
+JHtml::_( 'behavior.keepalive' );
+JHtml::_( 'behavior.tooltip' );
+JHtml::_( 'behavior.formvalidation' );
+JHtml::_( 'formbehavior.chosen', 'select' );
 
 //Load admin language file
 $lang = JFactory::getLanguage();
-$lang->load('com_swa', JPATH_ADMINISTRATOR);
+$lang->load( 'com_swa', JPATH_ADMINISTRATOR );
 $doc = JFactory::getDocument();
-$doc->addScript(JUri::base() . '/components/com_swa/assets/js/form.js');
+$doc->addScript( JUri::base() . '/components/com_swa/assets/js/form.js' );
 ?>
 
 <!--</style>-->
@@ -29,7 +29,7 @@ $doc->addScript(JUri::base() . '/components/com_swa/assets/js/form.js');
 <p>If you need to make any changes on fields that are disable please email webmaster@swa.co.uk!</p>
 
 <form id="form-member-details" method="post"
-      action="<?php echo JRoute::_('index.php?option=com_swa&task=memberdetails'); ?>"
+      action="<?php echo JRoute::_( 'index.php?option=com_swa&task=memberdetails' ); ?>"
       class="form-validate form-horizontal" enctype="multipart/form-data">
     <table class="table">
         <thead></thead>
@@ -39,60 +39,60 @@ $doc->addScript(JUri::base() . '/components/com_swa/assets/js/form.js');
             <td><?php echo $this->user->name; ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('id'); ?></td>
-            <td><?php echo $this->form->getInput('id'); ?></td>
+            <td><?php echo $this->form->getLabel( 'id' ); ?></td>
+            <td><?php echo $this->form->getInput( 'id' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('sex'); ?></td>
-            <td><?php echo $this->form->getInput('sex'); ?></td>
+            <td><?php echo $this->form->getLabel( 'sex' ); ?></td>
+            <td><?php echo $this->form->getInput( 'sex' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('dob'); ?></td>
-            <td><?php echo $this->form->getInput('dob'); ?></td>
+            <td><?php echo $this->form->getLabel( 'dob' ); ?></td>
+            <td><?php echo $this->form->getInput( 'dob' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('tel'); ?></td>
-            <td><?php echo $this->form->getInput('tel'); ?></td>
+            <td><?php echo $this->form->getLabel( 'tel' ); ?></td>
+            <td><?php echo $this->form->getInput( 'tel' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('university_id'); ?></td>
-            <td><?php echo $this->form->getInput('university_id'); ?></td>
+            <td><?php echo $this->form->getLabel( 'university_id' ); ?></td>
+            <td><?php echo $this->form->getInput( 'university_id' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('course'); ?></td>
-            <td><?php echo $this->form->getInput('course'); ?></td>
+            <td><?php echo $this->form->getLabel( 'course' ); ?></td>
+            <td><?php echo $this->form->getInput( 'course' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('graduation'); ?></td>
-            <td><?php echo $this->form->getInput('graduation'); ?></td>
+            <td><?php echo $this->form->getLabel( 'graduation' ); ?></td>
+            <td><?php echo $this->form->getInput( 'graduation' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('discipline'); ?></td>
-            <td><?php echo $this->form->getInput('discipline'); ?></td>
+            <td><?php echo $this->form->getLabel( 'discipline' ); ?></td>
+            <td><?php echo $this->form->getInput( 'discipline' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('level'); ?></td>
-            <td><?php echo $this->form->getInput('level'); ?></td>
+            <td><?php echo $this->form->getLabel( 'level' ); ?></td>
+            <td><?php echo $this->form->getInput( 'level' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('shirt'); ?></td>
-            <td><?php echo $this->form->getInput('shirt'); ?></td>
+            <td><?php echo $this->form->getLabel( 'shirt' ); ?></td>
+            <td><?php echo $this->form->getInput( 'shirt' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('econtact'); ?></td>
-            <td><?php echo $this->form->getInput('econtact'); ?></td>
+            <td><?php echo $this->form->getLabel( 'econtact' ); ?></td>
+            <td><?php echo $this->form->getInput( 'econtact' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('enumber'); ?></td>
-            <td><?php echo $this->form->getInput('enumber'); ?></td>
+            <td><?php echo $this->form->getLabel( 'enumber' ); ?></td>
+            <td><?php echo $this->form->getInput( 'enumber' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('dietary'); ?></td>
-            <td><?php echo $this->form->getInput('dietary'); ?></td>
+            <td><?php echo $this->form->getLabel( 'dietary' ); ?></td>
+            <td><?php echo $this->form->getInput( 'dietary' ); ?></td>
         </tr>
         <tr>
-            <td><?php echo $this->form->getLabel('swahelp'); ?></td>
-            <td><?php echo $this->form->getInput('swahelp'); ?></td>
+            <td><?php echo $this->form->getLabel( 'swahelp' ); ?></td>
+            <td><?php echo $this->form->getInput( 'swahelp' ); ?></td>
         </tr>
 
         <tr>
@@ -100,12 +100,12 @@ $doc->addScript(JUri::base() . '/components/com_swa/assets/js/form.js');
                 <div class="control-group">
                     <div class="controls">
                         <button type="submit" class="validate btn btn-primary">
-                            <?php echo JText::_('JSUBMIT'); ?>
+                            <?php echo JText::_( 'JSUBMIT' ); ?>
                         </button>
                         <a class="btn"
-                           href="<?php echo JRoute::_('index.php?option=com_swa&task=memberdetails.cancel'); ?>"
-                           title="<?php echo JText::_('JCANCEL'); ?>">
-                            <?php echo JText::_('JCANCEL'); ?>
+                           href="<?php echo JRoute::_( 'index.php?option=com_swa&task=memberdetails.cancel' ); ?>"
+                           title="<?php echo JText::_( 'JCANCEL' ); ?>">
+                            <?php echo JText::_( 'JCANCEL' ); ?>
                         </a>
                     </div>
                 </div>
@@ -116,5 +116,5 @@ $doc->addScript(JUri::base() . '/components/com_swa/assets/js/form.js');
 
     <input type="hidden" name="option" value="com_swa"/>
     <input type="hidden" name="task" value="memberdetails.submit"/>
-    <?php echo JHtml::_('form.token'); ?>
+    <?php echo JHtml::_( 'form.token' ); ?>
 </form>
