d6a32920c00bd579cca2bb0049976594b7b10daf
addshore <addshorewiki@gmail.com>
2020-12-29 16:46:19 +0000

    images.sh don't empty the images dir

--- .build/images.sh
+++ .build/images.sh
@@ -3,9 +3,7 @@
 
 echo 'Fetching images'
 
-rm -rf ./.docker/www/images
-mkdir -p ./.docker/www/images
-mkdir -p ./.docker/www/images/FavouriteTemplate
-
 curl -L --progress-bar -o ./.docker/www/images/SWA_Vector.png 'https://studentwindsurfing.co.uk/images/SWA_Vector.png'
+
+mkdir -p ./.docker/www/images/FavouriteTemplate
 curl -L --progress-bar -o ./.docker/www/images/FavouriteTemplate/body.jpg 'https://studentwindsurfing.co.uk/images/FavouriteTemplate/body.jpg'
