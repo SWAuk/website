52dd38f5baf97635c024e758709fb1133e432ba3
addshore <addshorewiki@gmail.com>
2020-12-29 16:06:10 +0000

    build: Don't fetch Joomla if you already have it

--- .build/joomla.sh
+++ .build/joomla.sh
@@ -2,12 +2,24 @@
 # Download the appropriate version of Joomla from Github that we want to develop against.
 # This should generally be kept in sync with what is running in production.
 
-echo 'Fetching Joomla'
-echo 'Sorry this is a bit slow currently...'
-# TODO make it skip the download if we already have the right version..
-
 # New version tags can be gotten from https://github.com/joomla/joomla-cms/releases
-curl --progress-bar -L -o joomla.zip 'https://github.com/joomla/joomla-cms/archive/3.9.23.zip'
+export joomtoget="https://github.com/joomla/joomla-cms/archive/3.9.23.zip"
+touch .joomgot
+
+# Only download Joomla if we havn't already got it
+if grep -q $joomtoget .joomgot; then
+  if test -f "./.docker/www/index.php"; then
+    echo 'Skiping Joomla fetch, as current version seems up to date...'
+    echo 'If you need to force a refetch then delete the .joomgot file'
+    exit 0
+  fi
+fi
+
+echo $joomtoget > .joomgot
+
+echo 'Fetching Joomla'
+
+curl --progress-bar -L -o joomla.zip $joomtoget
 
 rm -rf ./.docker/www
 mkdir -p ./.docker/www
