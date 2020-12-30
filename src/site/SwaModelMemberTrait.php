aee7f52e77a5d83fd6066ee3e2ff537ccdc94c12
oshotton <geekman92@gmail.com>
2020-12-30 18:11:12 +0000

    Fix SwaModelMemberTrait after rebase

--- src/site/SwaModelMemberTrait.php
+++ src/site/SwaModelMemberTrait.php
@@ -21,26 +21,20 @@ trait SwaModelMemberTrait
 			$user  = JFactory::getUser();
 
 			// Select the required fields from the table.
-			$query->select( 'a.*' );
+			$query->select( 'member.*' );
 			$query->select( '!ISNULL(committee.id) AS swa_committee' );
 			$query->select( 'membership.committee AS club_committee' );
-			$query->select( 'membership.uni_id AS uni_id' );
-			$query->select( 'membership.season_id' );
+			$query->select( 'uni.id AS uni_id' );
+			$query->select( 'uni.name AS university_name' );
+			$query->select( 'season.id AS season_id' );
+			$query->select('season.year AS season_year');
 
-			$query->from( '#__swa_member AS a' );
-			$query->leftJoin( '#__swa_committee AS committee ON committee.member_id = a.id');
-			$query->leftJoin( '#__swa_membership AS membership on membership.member_id = a.id' );
+			$query->from( '#__swa_member AS member' );
+			$query->leftJoin( '#__swa_committee AS committee ON committee.member_id = member.id');
+			$query->leftJoin( '#__swa_membership AS membership on membership.member_id = member.id' );
+			$query->leftJoin( '#__swa_university AS uni ON uni.id = membership.uni_id' );
 			$query->leftJoin( '#__swa_season AS season ON season.id = membership.season_id' );
 
-			// Join on university table
-			$query->leftJoin('#__swa_university AS uni ON uni_member.university_id = uni.id');
-			$query->select('uni.name AS university_name');
-
-			// Join on membership table
-			$query->leftJoin('#__swa_membership AS membership ON membership.member_id = a.id');
-			$query->select('membership.season_id');
-			$query->leftJoin('#__swa_season AS season ON membership.season_id = season.id');
-			$query->select('season.year AS season_year');
 			$query->order('season.year DESC');
 
 			// Load the result
