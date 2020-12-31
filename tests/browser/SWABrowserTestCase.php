2658fb3f4f89f72a46d3f080be6870dd43097e11
addshore <addshorewiki@gmail.com>
2020-12-31 16:41:42 +0000

    Run browser tests on Github actions

--- tests/browser/SWABrowserTestCase.php
+++ tests/browser/SWABrowserTestCase.php
@@ -8,32 +8,74 @@ use Facebook\WebDriver\Remote\RemoteWebDriver;
 
 abstract class SWABrowserTestCase extends TestCase
 {
+	private const HOST_DOCKER_INTERNAL = 'host.docker.internal';
+
 	/**
 	 * @var RemoteWebDriver
 	 */
 	protected $webDriver;
 
 	/**
-	 * TODO make configurable for different users?
 	 * @var string
 	 */
-	protected $baseUrl = 'http://host.docker.internal:5555';
+	protected $baseUrl;
 
-	public function getCapabilities()
-    {
+	private function hostExists( $host ) : bool
+	{
+		return gethostbyname($host) != $host;
+	}
+
+	private function detectSeleniumHost() : string
+	{
+		// TODO also make configurable
+		// If test is being run in general docker container, expect to connect to the host.
+		if ($this->hostExists(self::HOST_DOCKER_INTERNAL)) {
+			return self::HOST_DOCKER_INTERNAL;
+		}
+		// If the magic internal host doesn't exist then localhost should suffice.
+		return 'localhost';
+	}
+
+	private function setBaseUrl() : void
+	{
+		// TODO also make configurable
+		// The default configuration for browser tests runs joomla and selenium in docker-compose, so this should work.
+		$this->baseUrl = 'http://joomla:80';
+	}
+
+	private function setWebDriver() : void
+	{
 		$capabilities = DesiredCapabilities::chrome();
-		return $capabilities;
+		$seleniumUrl = 'http://' . $this->detectSeleniumHost() . ':4444/wd/hub';
+		$this->webDriver = RemoteWebDriver::create($seleniumUrl, $capabilities);
+	}
+
+	private function setWebDriverWithRetries( $attempts = 15 )
+	{
+		try{
+			$attempts--;
+			$this->setWebDriver();
+		}
+		catch (\TypeError $e){
+			$failMsg = 'Argument 1 passed to Facebook\WebDriver\Remote\DesiredCapabilities::__construct() must be of the type array, null given';
+			if (strstr($e->getMessage(), $failMsg)) {
+				if ($attempts <= 0) {
+					throw new \RuntimeException("Seemingly couldn't connect to selenium.");
+				}
+				sleep(2);
+				$this->setWebDriverWithRetries($attempts);
+			}
+		}
 	}
 
 	public function setUp() : void
-    {
-		$capabilities = $this->getCapabilities();
-		// Connect to the remote webdriver that is served via docker
-		$this->webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $capabilities);
+	{
+		$this->setWebDriverWithRetries();
+		 $this->setBaseUrl();
 	}
 
 	public function tearDown(): void
-    {
+	{
 		$this->webDriver->quit();
 	}
 }
