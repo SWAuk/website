d1daa1cc19289035be81a93f5867a274ca3dd434
James Arney <jamesarney@outlook.com>
2021-01-10 16:13:59 +0000

    added documentation to payment files

--- src/site/views/memberpayment/tmpl/default.php
+++ src/site/views/memberpayment/tmpl/default.php
@@ -1,3 +1,10 @@
+<!-- 
+This payment process is based off the 'Custom Payment Flow' at the following stipe site. 
+https://stripe.com/docs/payments/accept-a-payment?integration=elements
+
+A tutorial is available here (at the time of writing):
+https://stripe.com/docs/payments/integration-builder
+-->
 <?php
 
 defined('_JEXEC') or die;
