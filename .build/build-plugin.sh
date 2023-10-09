96ce2927e28cc2dec85f7deb4cda98f7943db855
Oscar L <oscarlindenbaum@gmail.com>
2023-10-09 15:28:54 +0100

    attempted to speed up build

--- .build/build-plugin.sh
+++ .build/build-plugin.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-# Create a built .ZIP file that can be deployed to prodction
+# Create a built .ZIP file that can be deployed to production
 
 # Cleanup and setup
-rm -rf plg_swa_viewlevels.zip
+echo "Cleaning up previous build artifacts..."
+rm -f plg_swa_viewlevels.zip
 mkdir -p ./.build/output-plugin
 
 # Copy the code we are zipping into a tmp dir
+echo "Copying plugin code into a temporary directory..."
 cp -r ./src/plugin/ ./.build/output-plugin
+echo "Plugin code copied successfully."
 
 # Zip it up correctly
-cd ./.build/output-plugin/ ; zip -r ./../../plg_swa_viewlevels.zip ./* ; cd ../..
+echo "Zipping the plugin code..."
+cd ./.build/output-plugin/
+zip -rq ./../../plg_swa_viewlevels.zip ./*
+cd ../..
+echo "Plugin code zipped successfully."
 
 # Remove the temporary files
+echo "Removing temporary files..."
 rm -rf ./.build/output-plugin
+echo "Temporary files removed."
+
+echo "Build complete."
