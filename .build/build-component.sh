cf42482595b1d70c27bd8dc8fe487c478fd1429f
Oscar L <oscarlindenbaum@gmail.com>
2023-10-10 15:10:56 +0100

    Revert "attempted to speed up build"
    This reverts commit 96ce2927e28cc2dec85f7deb4cda98f7943db855.

--- .build/build-component.sh
+++ .build/build-component.sh
@@ -1,46 +1,19 @@
 #!/bin/bash
-# Create a built .ZIP file that can be deployed to production
+# Create a built .ZIP file that can be deployed to prodction
 
 # Cleanup and setup
-rm -f com_swa.zip
+rm -rf com_swa.zip
 mkdir -p ./.build/output-component/administrator
 mkdir -p ./.build/output-component/site
 
 # Copy the code we are zipping into a tmp dir and move some stuff around
-echo "Copying files into temporary directories..."
 cp -r ./src/administrator/* ./.build/output-component/administrator
 cp -r ./src/site/* ./.build/output-component/site
 mv ./.build/output-component/administrator/swa.xml ./.build/output-component/swa.xml
-echo "Files copied successfully."
 
-# Create a list of directories to zip
-directories_to_zip=(
-  ./.build/output-component/administrator
-  ./.build/output-component/site
-)
+# Zip it up correctly
+cd ./.build/output-component/ ; zip -r ./../../com_swa.zip ./* ; cd ../..
 
-# Function to zip a directory
-zip_directory() {
-  local dir="$1"
-  echo "Zipping directory: $dir..."
-  zip -rq0 "${dir}.zip" "${dir}"/*
-  echo "Directory zipped: $dir"
-}
+# Remove the temporary files
 
-export -f zip_directory
-
-# Use xargs to zip directories in parallel
-echo "Zipping directories in parallel..."
-printf "%s\n" "${directories_to_zip[@]}" | xargs -I {} -P $(nproc) -n 1 bash -c 'zip_directory "$1"' _ {}
-echo "Directories zipped successfully."
-
-# Merge the zip files into one
-echo "Merging zip files into com_swa.zip..."
-zip -qj com_swa.zip ./.build/output-component/*.zip
-echo "Merged zip files into com_swa.zip successfully."
-
-# Remove the temporary zip files
-echo "Removing temporary zip files..."
-rm -rf ./.build/output-component/*.zip
 rm -rf ./.build/output-component
-echo "Temporary zip files removed."
--- .build/build-plugin.sh
+++ .build/build-plugin.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
-# Create a built .ZIP file that can be deployed to production
+# Create a built .ZIP file that can be deployed to prodction
 
 # Cleanup and setup
-echo "Cleaning up previous build artifacts..."
-rm -f plg_swa_viewlevels.zip
+rm -rf plg_swa_viewlevels.zip
 mkdir -p ./.build/output-plugin
 
 # Copy the code we are zipping into a tmp dir
-echo "Copying plugin code into a temporary directory..."
 cp -r ./src/plugin/ ./.build/output-plugin
-echo "Plugin code copied successfully."
 
 # Zip it up correctly
-echo "Zipping the plugin code..."
-cd ./.build/output-plugin/
-zip -rq ./../../plg_swa_viewlevels.zip ./*
-cd ../..
-echo "Plugin code zipped successfully."
+cd ./.build/output-plugin/ ; zip -r ./../../plg_swa_viewlevels.zip ./* ; cd ../..
 
 # Remove the temporary files
-echo "Removing temporary files..."
 rm -rf ./.build/output-plugin
-echo "Temporary files removed."
-
-echo "Build complete."
